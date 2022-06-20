9195dbbefb39f58091b521fb61bbc8c67dff9139
ukyo0715 <ukyoo0715@gmail.com>
2022-06-20 14:05:01 +0900

    second

--- app/app.py
+++ app/app.py
@@ -3,12 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object('config')
 
-db = SQLAlchemy(app) 
-from models import user
-
-import views
 
 if __name__ == '__main__':
     app.run(host='localhost')
